803f3d5ecb9192442874328c248437972acf14a2
Dylan Deng <dxx1104@gmail.com>
2019-11-03 16:17:21 +0800

    part of V02/C06 and V01/C16, require.h

---
+++ V01/C16/fibonacci.cpp
@@ -0,0 +1,19 @@
+//: C16fibonacci.cpp {0}
+#include "../require.h"
+
+int fibonacci(int n) {
+  const int sz = 100;
+  require(n < sz);
+  static int f[sz];  // initialized to zero
+  f[0] = f[1] = 1;
+  // Scan for unified array elements:
+  int i;
+  for (i = 0; i < sz; i++) {
+    if (f[i] == 0) break;
+  }
+  while (i <= n) {
+    f[i] = f[i - 1] + f[i - 2];
+    i++;
+  }
+  return f[n];
+}  ///:~
---
+++ V02/C06/SortedSearchTest.cpp
@@ -0,0 +1,20 @@
+//: C06:SortedSearchTest.cpp
+// Test searching in sorted ranges.
+// NString
+#include <algorithm>
+#include <cassert>
+#include <ctime>
+#include <cstdlib>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include "NString.h"
+#include "PrintSequence.h"
+#include "../require.h"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+}
